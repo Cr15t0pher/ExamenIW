53232b5fc076f435cfc4c740f6f0a3ae3ebf89c5
Cr15t0pher <18170344@itculiacan.edu.mx>
2023-03-30 09:39:02 -0700

    Leer todos los clientes

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Leer todos
+app.get('/clientes', (req, res) => {
+    connection.query('SELECT * FROM clientes', (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+
 app.listen(3000, () => {
     console.log('API escuchando en el puerto 3000');
   });
